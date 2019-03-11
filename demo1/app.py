5c1fe8755b062078b7b544fe8688425469d26b0d
GameChaner <941994464@qq.com>
2019-03-11 11:02:26 +0800

    choose good pic

--- demo1/app.py
+++ demo1/app.py
@@ -8,7 +8,8 @@ import os
 HANDLERS = [
     (r"/api/users", user_handlers.UserListHandler),
     (r"/",showPic.ShowPicHandler),
-    (r"/select",showPic.Select_picHandler)
+    (r"/select",showPic.Select_picHandler),
+    (r"/start",showPic.StartHandler)
 ]
 # settings = {
 #     "static_path":"D:/Git_repository/Narrative-flow-web/demo1/data/pic_data"
